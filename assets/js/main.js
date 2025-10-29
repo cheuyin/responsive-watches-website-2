3beef39c2ef02ca288c10de0086dfef1b4d4886d
cheuyin <yinstanleycheung@gmail.com>
2025-10-29 10:11:05 -0700

    Create mobile menu with toggle

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,18 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
 
+navToggle?.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+});
+
+navClose?.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+});
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
-
 /*=============== SWIPER WACTHES ===============*/
-
 
 /*=============== GSAP ANIMATION ===============*/
