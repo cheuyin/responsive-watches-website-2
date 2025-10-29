3fbedade70d760a56e6758f44cff5830577c7b97
cheuyin <yinstanleycheung@gmail.com>
2025-10-29 10:15:55 -0700

    Hide menu in mobile whenever a link is clicked

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,6 +12,14 @@ navClose?.addEventListener("click", () => {
 });
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll(".nav__link");
+
+const linkAction = () => {
+    const navMenu = document.getElementById("nav-menu");
+    navMenu.classList.remove("show-menu");
+};
+
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== SWIPER WACTHES ===============*/
 
