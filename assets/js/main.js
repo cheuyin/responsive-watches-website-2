3772bbec8461eaaeadbacb8cfa8e9e5e1210f7af
cheuyin <yinstanleycheung@gmail.com>
2025-10-29 14:09:09 -0700

    Add swipeable watch images

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,23 +4,45 @@ const navToggle = document.getElementById("nav-toggle");
 const navClose = document.getElementById("nav-close");
 
 navToggle?.addEventListener("click", () => {
-    navMenu.classList.add("show-menu");
+  navMenu.classList.add("show-menu");
 });
 
 navClose?.addEventListener("click", () => {
-    navMenu.classList.remove("show-menu");
+  navMenu.classList.remove("show-menu");
 });
 
 /*=============== REMOVE MENU MOBILE ===============*/
 const navLink = document.querySelectorAll(".nav__link");
 
 const linkAction = () => {
-    const navMenu = document.getElementById("nav-menu");
-    navMenu.classList.remove("show-menu");
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
 };
 
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
-/*=============== SWIPER WACTHES ===============*/
+/*=============== SWIPER WATCHES ===============*/
+const swiperWatches = new Swiper(".home__swiper", {
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  spaceBetween: 32,
+  grabCursor: true,
+  effect: "creative",
+  creativeEffect: {
+    prev: {
+      translate: [-100, 0, -500],
+      rotate: [0, 0, 15],
+      opacity: 0,
+    },
+    next: {
+      translate: [100, 0, -500],
+      rotate: [0, 0, -15],
+      opacity: 0,
+    },
+  },
+});
 
 /*=============== GSAP ANIMATION ===============*/
